2fec2bd0dfc42148fd7a13bc8243ed1adfa10065
Conrad Osvik <conrad.tinius.osvik@gmail.com>
2024-09-19 11:09:20 +0200

    add ignores

--- forprosjekt/main.py
+++ forprosjekt/main.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
@@ -16,15 +15,15 @@ slope, intercept, r_value, p_value, std_err = linregress(X, Y)
 
 print(f"Regresjonslinje: y = {intercept:.2f} + {slope:.2f}x")
 
-print(f"R-squared: {r_value**2:.2f}") # type: ignore
+print(f"R-squared: {r_value**2:.2f}")  # type: ignore
 
-plt.scatter(X, Y, color='blue', label='Data')  
-plt.plot(X, intercept + slope * X.astype(float), color='red',
-         label='Regresjonslinje') 
+plt.scatter(X, Y, color='blue', label='Data')  # type: ignore
+plt.plot(X, intercept + slope * X.astype(float), color='red',  # type: ignore
+         label='Regresjonslinje')
 plt.xlabel('Skostørrelse')
 plt.ylabel('Høyde (cm)')
 plt.title('Skostørrelse vs. Høyde')
 plt.legend()
 
 plt.savefig('regresjonsplot.pdf')
-plt.show()  
+plt.show()
